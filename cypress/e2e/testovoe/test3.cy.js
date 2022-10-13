ade33c27587c8ef2b61571dcef7deae9f895c857
Sergey Andreev <ozwatandree@gmail.com>
2022-10-13 23:57:40 +0300

    test ready

--- cypress/e2e/testovoe/test3.cy.js
+++ cypress/e2e/testovoe/test3.cy.js
@@ -5,9 +5,9 @@ const psw = 'SS21%aaW';
 const google = 'https://www.google.ru/';
 
 
-describe('Assert',()=>{
+describe('test site',()=>{
 
-    it('Assert web tesr',()=>{
+    it('test google find',()=>{
         cy.viewport(1920,1080);
         cy.visit(google);
         cy.location('protocol').should('eq','https:');
@@ -16,6 +16,17 @@ describe('Assert',()=>{
         
 
     })
+
+    it('site contact',()=>{
+        cy.viewport(1920,1080);
+        cy.visit('https://byndyusoft.com/');
+        
+        cy.scrollTo(0,4500);
+        cy.get('.know-more__container > .btn').should('have.text','\n            Заказать презентацию\n        ').click();
+        cy.xpath('/html/body/div[8]/div[2]/div[2]/div/a').should('have.text','sales@byndyusoft.com')
+        //телефон просто отсутствует поэтому я немогу проверить его наличие (тк даже не предстовляю где он находится)
+    })
+
 })
 
 
